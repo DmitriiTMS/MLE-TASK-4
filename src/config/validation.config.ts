54ff651770913438ac89c5cced1ea60ddbfbb212
DmitriiTMS <dmitrii12_88@mail.ru>
2026-04-30 18:35:21 +0300

    feat: add app.config, validation config

---
+++ src/config/validation.config.ts
@@ -0,0 +1,38 @@
+import { plainToInstance } from 'class-transformer';
+import { validateSync, ValidationError } from 'class-validator';
+import { ApplicationConfig } from './app.config';
+
+function formatErrors(errors: ValidationError[]): string {
+    return errors.map(error => {
+        if (error.constraints) {
+            return `${error.property}: ${Object.values(error.constraints).join(', ')}`;
+        }
+        if (error.children && error.children.length) {
+            return formatErrors(error.children);
+        }
+        return '';
+    }).filter(Boolean).join('\n');
+}
+
+export function validateConfig(config: Record<string, unknown>) {
+    console.log('🔍 Валидация конфигурации...');
+
+    const validatedConfig = plainToInstance(ApplicationConfig, config, {
+        enableImplicitConversion: true,
+    });
+
+    const errors = validateSync(validatedConfig, {
+        skipMissingProperties: false,
+        validationError: { target: false, value: true }, 
+        forbidUnknownValues: true,
+    });
+
+    if (errors.length > 0) {
+        const errorMessage = formatErrors(errors);
+        console.error('❌ Ошибки валидации:\n', errorMessage);
+        throw new Error(`Config validation error:\n${errorMessage}`);
+    }
+
+    console.log('✅ Конфигурация валидна');
+    return validatedConfig;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+  const config = app.get(ConfigService);
+
+  const port = config.getOrThrow<number>('HTTP_PORT');
+  const host = config.getOrThrow<string>('HTTP_HOST');
+
+  app.setGlobalPrefix(config.getOrThrow<string>('HTTP_PREFIX'));
+  await app.listen(port, host);
+
+  Logger.log(`🚀 Application is running on: ${await app.getUrl()}`)
+  Logger.log(`📍 Environment: ${config.get('NODE_ENV')}`)
 }
 bootstrap();
