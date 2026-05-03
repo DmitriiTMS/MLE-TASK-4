2552be5ef84d415cb0ba18192e4e3baae9b468b0
DmitriiTMS <dmitrii12_88@mail.ru>
2026-05-03 14:44:01 +0300

    feat: add userEntit

--- src/modules/database/migrations/index.ts
+++ src/modules/database/migrations/index.ts
@@ -1,3 +1,4 @@
 import { AddUserEntity1777626621479 } from './1777626621479-AddUserEntity';
+import { RenamePasswordColumnUserEntity1777808168800 } from './1777808168800-RenamePasswordColumnUserEntity';
 
-export default [AddUserEntity1777626621479];
+export default [AddUserEntity1777626621479, RenamePasswordColumnUserEntity1777808168800];
--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -11,18 +11,26 @@ export class UserEntity {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column()
+    @Column({ type: 'varchar', length: 255, nullable: false })
     name: string;
 
-    @Column({ unique: true })
+    @Column({ unique: true, length: 255, nullable: false })
     email: string;
 
-    @Column()
-    password: string;
+    @Column({ type: 'varchar', name: 'password_hash', nullable: false, select: false })
+    passwordHash: string;
 
-    @CreateDateColumn()
+    @CreateDateColumn({ type: 'timestamptz', name: 'created_at', nullable: false })
     createdAt: Date;
 
-    @UpdateDateColumn()
+    @UpdateDateColumn({ type: 'timestamptz', name: 'updated_at', nullable: false })
     updatedAt: Date;
+
+    static createInstance(name: string, email: string, passwordHash: string): UserEntity {
+        const user = new UserEntity();
+        user.name = name;
+        user.email = email;
+        user.passwordHash = passwordHash;
+        return user;
+    }
 }
